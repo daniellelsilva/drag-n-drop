abf4ca66d11dedf54614b08a0a236e8d1ade5942
Danielle Silva <danielle.luisasilva@gmail.com>
2022-03-09 18:48:21 -0300

    adiciona provider no app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import TableProvider from './context/TableProvider';
+import DragNDrop from './pages/DragNDrop';
+import Table from './pages/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TableProvider>
+      <DragNDrop />
+      <Table />
+    </TableProvider>
   );
 }
 
