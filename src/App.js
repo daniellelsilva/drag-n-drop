53c5562f0e7d6f7e9971f5eef0dc191b174f7c37
Danielle Silva <danielle.luisasilva@gmail.com>
2022-03-11 11:40:48 -0300

    muda provider para index

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.scss';
-import TableProvider from './context/TableProvider';
+
 import DragNDrop from './components/DragNDrop';
 import Table from './components/Table';
 import Header from './components/Header';
 
 function App() {
   return (
-    <TableProvider>
+    <>
       <Header />
       <DragNDrop />
       <Table />
-    </TableProvider>
+    </>
   );
 }
 
